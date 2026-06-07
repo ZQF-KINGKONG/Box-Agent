80908f81fd2e872e8db336a2aa79b65df597a909
malin1 <malin1@sensetime.com>
2026-06-08 01:23:15 +0800

    chore: release v0.8.65

--- box_agent/__init__.py
+++ box_agent/__init__.py
@@ -6,7 +6,7 @@ from .hooks import BaseHook, HookManager, load_hooks
 from .llm import LLMClient
 from .schema import FunctionCall, LLMProvider, LLMResponse, Message, ToolCall
 
-__version__ = "0.8.64"
+__version__ = "0.8.65"
 
 __all__ = [
     "Agent",

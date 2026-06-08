44c7a4b24834bd4bd156aed3071b59c47ffd7a03
malin1 <malin1@sensetime.com>
2026-06-08 19:26:26 +0800

    chore: bump version to 0.8.68

--- box_agent/__init__.py
+++ box_agent/__init__.py
@@ -6,7 +6,7 @@ from .hooks import BaseHook, HookManager, load_hooks
 from .llm import LLMClient
 from .schema import FunctionCall, LLMProvider, LLMResponse, Message, ToolCall
 
-__version__ = "0.8.67"
+__version__ = "0.8.68"
 
 __all__ = [
     "Agent",
